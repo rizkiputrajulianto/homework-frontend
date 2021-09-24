9c8b0be1298a5b8b9f453cbb202e92e110d34be2
Rizki Putra Julianto <rizkiputrajulianto111@gmail.com>
2021-09-24 19:47:45 +0700

    add stisla

---
+++ components/partials/footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <footer class="main-footer">
+      <div class="footer-left">
+        Copyright &copy; 2021
+        <div class="bullet"></div>
+        Design By <a href="https://nauval.in/">Student</a>
+      </div>
+      <div class="footer-right">2.3.0</div>
+    </footer>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div id="app">
+      <div class="main-wrapper">
+        <div class="navbar-bg"></div>
+        <partials-navbar />
+        <partials-sidebar />
+
+        <!-- Main Content -->
+        <div class="main-content">
+          <section class="section">
+            <div class="section-header">
+              <h1>Blank Page</h1>
+            </div>
+
+            <div class="section-body"></div>
+          </section>
+        </div>
+        <partials-footer />
+      </div>
+    </div>
+  </div>
+</template>
