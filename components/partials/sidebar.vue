bcd47dbe2e912491cb9be17a583ba441a9033103
ナイラ <adekuswandi@Nailahs-MacBook-Pro.local>
2021-10-09 10:06:00 +0700

    sidebar

--- components/partials/sidebar.vue
+++ components/partials/sidebar.vue
@@ -8,22 +8,29 @@
           <a href="index.html">FS</a>
         </div>
         <ul class="sidebar-menu">
-          <li class="menu-header">Dashboard</li>
-          <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"
-              ><i class="fas fa-fire"></i><span>Dashboard</span></a
-            >
-            <ul class="dropdown-menu">
-              <li>
-                <a class="nav-link" href="index-0.html">General Dashboard</a>
-              </li>
-              <li>
-                <a class="nav-link" href="index.html">Ecommerce Dashboard</a>
-              </li>
-            </ul>
+          <li v-for="(menu,i) in sbMenu" :key="i">
+            <nuxt-link class="nav-link" :to="menu.to">
+              <i :class="menu.icon"></i> <span>{{menu.name}}</span>
+            </nuxt-link>
           </li>
         </ul>
         
       </aside>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      sbMenu: [
+        {
+          name: "Semua Kelas",
+          icon: "fas fa-home",
+          to: "/"
+        }
+      ]
+    }
+  },
+}
+</script>
