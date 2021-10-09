4f5bf638d988451a874e3b0caf1489dc128c0862
ナイラ <adekuswandi@Nailahs-MacBook-Pro.local>
2021-10-09 13:28:51 +0700

    sidebar

--- components/partials/sidebar.vue
+++ components/partials/sidebar.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="main-sidebar">
-      <aside id="sidebar-wrapper">
-        <div class="sidebar-brand">
-          <a href="index.html">Fullstack Academy</a>
-        </div>
-        <div class="sidebar-brand sidebar-brand-sm">
-          <a href="index.html">FS</a>
-        </div>
-        <ul class="sidebar-menu">
-          <li v-for="(menu,i) in sbMenu" :key="i">
-            <nuxt-link class="nav-link" :to="menu.to">
-              <i :class="menu.icon"></i> <span>{{menu.name}}</span>
-            </nuxt-link>
-          </li>
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html">Fullstack</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">FS</a>
+      </div>
+      <ul class="sidebar-menu">
+        <li v-for="(menu,i) in listMenu" :key="i">
+          <nuxt-link class="nav-link" :to="menu.to">
+            <i :class="menu.icon"></i> <span>{{menu.name}}</span>
+          </nuxt-link>
+        </li>
         </ul>
         
       </aside>
@@ -23,11 +23,11 @@
 export default {
   data() {
     return {
-      sbMenu: [
+      listMenu: [
         {
           name: "Semua Kelas",
           icon: "fas fa-home",
-          to: "/"
+          to: "/all-class"
         }
       ]
     }
