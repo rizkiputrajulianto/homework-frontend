54d0fad7ac92b97d19f6b317b9933f9749ef0269
ナイラ <adekuswandi@Nailahs-MacBook-Pro.local>
2021-10-10 23:38:08 +0700

    presensi

--- components/partials/sidebar.vue
+++ components/partials/sidebar.vue
@@ -37,7 +37,7 @@ export default {
         {
           name: "Presensi",
           icon: "fas fa-user",
-          to: "/"
+          to: "/presensi"
         },
       ]
     }
---
+++ pages/presensi/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <h1>SPV</h1>
+    <div class="row">
+      <div class="col-md-4" v-for="(user,i) in listTeacher" :key="i">
+        <class-item :name="user.name" :desc="user.desc" />
+      </div>
+    </div>
+    <h1>Student</h1>
+    
+    <div class="row">
+      <div class="col-md-4" v-for="(user,i) in listStudent" :key="i">
+        <class-item :name="user.name" :desc="user.desc" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  asyncData() {
+    return {
+      listTeacher: [
+        { name: "student", desc: "desc" },
+        { name: "student", desc: "desc" },
+      ],
+      listStudent: [
+        { name: "student", desc: "desc" },
+        { name: "student", desc: "desc" },
+        { name: "student", desc: "desc" },
+        { name: "student", desc: "desc" },
+        { name: "student", desc: "desc" },
+      ],
+    };
+  },
+};
+</script>
