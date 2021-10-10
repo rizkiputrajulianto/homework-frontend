d980d34d7c08330b750adeb320a044e930bd5185
ナイラ <adekuswandi@Nailahs-MacBook-Pro.local>
2021-10-10 21:43:18 +0700

    list kelas

---
+++ components/class/item.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+    <img :src="img" class="card-img-top" :alt="'foto ' +name">
+        <h5 class="card-title">{{name}}</h5>
+      <p class="card-text">{{desc}}</p>
+<button type="button" class="btn btn-primary" onclick="id" data-toggle="modal" data-target="#exampleModal">
+  Details
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Detail</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h4>deskripsi</h4>
+        <p>{{desc}}</p>
+        <h4>Jadwal</h4>
+        <p>{{start}}</p>
+        <p>{{end}}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>      
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      default: "Nama Kelas",
+    },
+    desc: {
+      type: String,
+      default: "Ini deskripsi kelasnya ya",
+    },
+    img: {
+      type: String,
+      default: "https://i.pinimg.com/originals/e5/14/ed/e514ed267f3416f81b7b538e8d08ab93.jpg",
+    },
+    start: {
+      type: String,
+      default: "01-01-2021"
+    },
+    end: {
+      type: String,
+      default: "31-12-2021"
+    }
+  },
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: "https://service-classroom.herokuapp.com"
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -97,9 +99,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 8000 // default: 3000
+
   }
 }
